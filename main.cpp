3fe4b5d69035d980f497d4f234510fe717a096b9
wowobaldet <88originalll@gmail.com>
2025-09-15 02:28:00 +0000

    nearly

--- main.cpp
+++ main.cpp
@@ -198,6 +198,7 @@ Schema FillSchema(string schema) {
 
 
 void CreateFiles(Schema database_schema) {
+    system(("rm -rf \"" + database_schema.name + '\"').c_str());
     system(("mkdir \"" + database_schema.name + "\"").c_str());
     Structure* runnerStruct = database_schema.tables;
     while(runnerStruct != nullptr){
@@ -335,107 +336,19 @@ void LeaveOnlySelected(SelTabs_node* tables){
     tables->table_data = selected_data;
 }
 
-void FROM(Structure* selected_colons, string input_data, string schema_name, string tables_from, SelectedTables& sel_tabl) {
+void FROM(Structure* selected_colons, string schema_name, string tables_from, SelectedTables& sel_tabl) {
     string tabl = "";
     istringstream iss_tables(tables_from);
     while(iss_tables >> tabl) {
         if (tabl[tabl.size()-1] == ',') tabl.pop_back();
         sel_tabl.PushTabl(tabl);
     }
-    sel_tabl.GetColons(selected_colons, schema_name);
+    if (selected_colons != nullptr) sel_tabl.GetColons(selected_colons, schema_name);
     SelTabs_node* runner = sel_tabl.head;
     while (runner != nullptr) {
         runner->table_data = GetData(runner, schema_name);
         runner = runner->next;
     }
-}
-
-void Compare(string& table_data, int& colon_num, string& data_to_compare_right) {
-    string line = "";
-    istringstream line_iss(table_data);
-    getline(line_iss, line, '\n');
-    string new_text = line + '\n';
-    while(getline(line_iss, line, '\n')){
-        int colon_runner = 0;
-        string word = "";
-        istringstream word_iss(line);
-        while(getline(word_iss, word, ',')){
-            if (colon_runner == colon_num) {
-                if (word == data_to_compare_right) {
-                    new_text += line + '\n';
-                    break;
-                }
-            }
-            colon_runner++;
-        }
-    }
-    new_text.pop_back();
-    table_data = new_text;
-}
-
-void Compare(string& table_data_left, int& colon_num_left, string& table_data_right, int& colon_num_right){
-    string line_left = "";
-    string line_right = "";
-    istringstream left_iss(table_data_left);
-    istringstream right_iss(table_data_right);
-    string new_text_left = "";
-    string new_text_right = "";
-    getline(left_iss, line_left, '\n');
-    getline(right_iss, line_right, '\n');
-}
-
-SelTabs_node* findTablAndNum (SelectedTables& sel_tabls, Structure* data_to_compare, string& filepath, int& colon_num){
-    SelTabs_node* runner = sel_tabls.head;
-    while (runner != nullptr) {
-        if(runner->tabl_name == data_to_compare->name) {
-            break;
-        }
-    }
-    if (runner == nullptr) {
-        throw string("don't have that table");
-    }
-    colon_num = sel_tabls.GetColonsNum(data_to_compare->colonms, filepath + '/' + runner->tabl_name);
-    return runner;
-}
-
-void Filter(SelectedTables& sel_tables, string& tabl_colon, string& aftersign_data, string& filepath) {
-    Structure* data_to_compare_left = new Structure();
-    data_to_compare_left->PushBack(ParseSelected(tabl_colon));
-    int colon_num = 0;
-    SelTabs_node* current_tabl = findTablAndNum(sel_tables, data_to_compare_left, filepath, colon_num);
-    if (aftersign_data[0] == '\'') {
-        int i = 1;
-        string word = "";
-        while(aftersign_data[i] != '\'') {
-            word += aftersign_data[i];
-            i++;
-        }
-        aftersign_data = word;
-        Compare(current_tabl->table_data, colon_num, aftersign_data);
-    } else {
-        Structure* data_to_compare_right;
-        data_to_compare_right->PushBack(ParseSelected(aftersign_data));
-        int colon_num_right = 0;
-        SelTabs_node* current_tabl_right = findTablAndNum(sel_tables, data_to_compare_right, filepath, colon_num_right);
-        
-    }
-}
-
-void WHERE(SelectedTables& sel_tabls, string input_data, string& filepath) {
-    istringstream iss(input_data);
-    string word = "";
-    while (word != "WHERE") iss >> word;
-    string tabl_colon = "";
-    string sign = "";
-    string aftersign_data = "";
-    iss >> tabl_colon;
-    iss >> sign;
-    iss >> aftersign_data;
-    if (sign != "=") {
-        throw string("not correct enter");
-        return;
-    }
-    Filter(sel_tabls, tabl_colon, aftersign_data, filepath);
 }
 
 void CrossJoin(string& text1, string text2) {
@@ -456,6 +369,168 @@ void CrossJoin(string& text1, string text2) {
     }
     final_res.pop_back();
     text1 = final_res;
+}
+
+string GetValue(string line, int colon_num){
+    istringstream line_iss(line);
+    string word = "";
+    while (getline(line_iss, word, ',')) {
+        if(colon_num <= 0) break;
+        colon_num--;
+    }
+    return word;
+}
+
+void Compare(string& table_data, int& colon_num, string& data_to_compare_right) {
+    string line = "";
+    istringstream line_iss(table_data);
+    getline(line_iss, line, '\n');
+    string new_text = line + '\n';
+    while(getline(line_iss, line, '\n')){
+        if(GetValue(line, colon_num) == data_to_compare_right){
+            new_text += line + '\n';
+        }
+    }
+    new_text.pop_back();
+    table_data = new_text;
+}
+
+void Compare(string& full_text, int colon_num_left, int colon_num_right){
+    string line = "";
+    istringstream text_iss(full_text);
+    getline(text_iss, line, '\n');
+    string new_text = line + '\n';
+    while(getline(text_iss, line, '\n')){
+        string word1 = GetValue(line, colon_num_left);
+        string word2 = GetValue(line, colon_num_right);
+        if(word1 == word2){
+            new_text += line + '\n';
+        }
+    }
+    full_text = new_text;
+}
+
+int findTablAndNum(string& full_text, string tabl_name, string colon_name){
+    string line = "";
+    istringstream text_iss(full_text);
+    getline(text_iss, line);
+    string word = "";
+    istringstream line_iss(line);
+    int num = 0;
+    while(getline(line_iss, word, ',')) {
+        if (word == tabl_name) {
+            break;
+        }
+        num++;
+    }
+    num++;
+    while(getline(line_iss, word, ',')) {
+        if (word == colon_name) return num;
+        num++;
+    }
+    return num;
+}
+
+void Filter(string& full_text, string& tabl_colon, string& aftersign_data) {
+    Structure* data_to_compare_left = new Structure;
+    data_to_compare_left->PushBack(ParseSelected(tabl_colon));
+    int colon_num  = findTablAndNum(full_text, data_to_compare_left->name + "_pk", data_to_compare_left->colonms);
+    if (aftersign_data[0] == '\'') {
+        int i = 1;
+        string word = "";
+        while(aftersign_data[i] != '\'') {
+            word += aftersign_data[i];
+            i++;
+        }
+        aftersign_data = word;
+        Compare(full_text, colon_num, aftersign_data);
+    } else {
+        Structure* data_to_compare_right = new Structure;
+        data_to_compare_right->PushBack(ParseSelected(aftersign_data));
+        int colon_num_right = findTablAndNum(full_text, data_to_compare_right->name + "_pk", data_to_compare_right->colonms);
+        Compare(full_text, colon_num, colon_num_right);
+    }
+}
+
+void Connect(string& orig_text, string new_text){
+    string connected_text = "";
+    string line_or = "";
+    istringstream iss_or(orig_text);
+    while (getline(iss_or, line_or, '\n')){
+        string line_new = "";
+        istringstream iss_new(new_text);
+        while(getline(iss_new, line_new, '\n')) {
+            if(line_or != line_new) {
+                connected_text += line_new + '\n';
+            }
+        }
+    }
+    orig_text += '\n' + connected_text;
+}
+
+void WHERE(string& full_text, string input_data) {
+    istringstream iss(input_data);
+    string word = "";
+    while (word != "WHERE") iss >> word;
+    string tabl_colon = "";
+    string sign = "";
+    string aftersign_data = "";
+    iss >> tabl_colon;
+    iss >> sign;
+    iss >> aftersign_data;
+    if (sign != "=") {
+        throw string("not correct enter");
+        return;
+    }
+    string full_text_copy = full_text;
+    Filter(full_text, tabl_colon, aftersign_data);
+    iss >> word;
+    while (word == "AND" || word == "OR"){
+        if (word == "AND") {
+            tabl_colon = "";
+            sign = "";
+            aftersign_data = "";
+            iss >> tabl_colon;
+            iss >> sign;
+            iss >> aftersign_data;
+            Filter(full_text, tabl_colon, aftersign_data);
+            if(!(iss >> word)) break;
+        }
+        if (word == "OR"){
+            tabl_colon = "";
+            sign = "";
+            aftersign_data = "";
+            iss >> tabl_colon;
+            iss >> sign;
+            iss >> aftersign_data;
+            string new_text = full_text_copy;
+            Filter(new_text, tabl_colon, aftersign_data);
+            Connect(full_text, new_text);
+            if(!(iss >> word)) break; 
+        }
+    }
+}
+
+void PrintFilteredSelected(string full_text, Structure* tables_colons){
+    SelTabs_node* nums_array = new SelTabs_node;
+    Structure* runner = tables_colons;
+    while(runner != nullptr) {
+        nums_array->PushNum(findTablAndNum(full_text, runner->name+"_pk", runner->colonms));
+        runner = runner->next;
+    }
+    string result = "";
+    string line = "";
+    istringstream line_iss(full_text);
+    while (getline(line_iss, line, '\n')){
+        ColonNums* nums_runner = nums_array->colons_head;
+        while (nums_runner != nullptr) {
+            result += GetValue(line, nums_runner->num) + ',';
+            nums_runner = nums_runner->next;
+        }
+        result.pop_back();
+        result += '\n';
+    }
+    cout << result << endl;
 }
 
 void PrintSelected(SelectedTables& sel_tabls){
@@ -488,9 +563,19 @@ void SELECT(string input_data, string schema_name){
         tables_from += data + ' ';
         if (!(iss >> data)) break;
     }
-    FROM(selected_colons, input_data, schema_name, tables_from, sel_tabl);
+    FROM(selected_colons, schema_name, tables_from, sel_tabl);
     if (data == "WHERE") {
-        WHERE(sel_tabl, input_data, schema_name);
+        string text = "";
+        SelTabs_node* runner = sel_tabl.head;
+        text = runner->table_data;
+        runner = runner->next;
+        while (runner != nullptr) {
+            CrossJoin(text, runner->table_data);
+            runner = runner->next;
+        }
+        WHERE(text, input_data);
+        PrintFilteredSelected(text, selected_colons);
+        return;
     }
     SelTabs_node* runner = sel_tabl.head;
     while (runner != nullptr) {
@@ -500,6 +585,74 @@ void SELECT(string input_data, string schema_name){
     PrintSelected(sel_tabl);
 }
 
+void InsertUpdatedData(string new_data, string filepath, int tuples_limit){
+    int order_num = 1;
+    while (isOverLimit (filepath, tuples_limit, order_num)) order_num++;
+    ofstream file_writer(filepath + "/" + to_string(order_num) + ".csv", ios_base::app);
+    if (!isEmpty(filepath + "/" + to_string(order_num) + ".csv")) {
+        file_writer << '\n' + new_data;
+        file_writer.close();
+        return;
+    };
+    file_writer << new_data;
+    file_writer.close();
+}
+
+
+void replaceTable(string new_text, string filepath, int tuples_limit) {
+    system(("rm "+filepath+"/*.csv").c_str());
+    istringstream text_iss(new_text);
+    string new_data = "";
+    while(getline(text_iss, new_data, '\n')){
+        InsertUpdatedData(new_data, filepath, tuples_limit);
+    }
+
+}
+
+bool checkLine(string line_or, string del_text){
+    string line_del = "";
+    istringstream del_iss(del_text);
+    bool isForADel = 0;
+    while(getline(del_iss, line_del, '\n')) {
+        if (line_or == line_del) {
+            isForADel = 1;
+            break;
+        }
+    }
+    return isForADel;
+}
+
+void CutOff(string& text_or, string del_text){
+    string line_or = "";;
+    istringstream or_iss(text_or);
+    string new_text = "";
+    getline(or_iss, line_or, '\n');
+    new_text += line_or + '\n';
+    while (getline(or_iss, line_or, '\n')) {
+        if(!checkLine(line_or, del_text)){
+            new_text += line_or + '\n';
+        }
+    }
+    text_or = new_text;
+}
+
+void DELETE(string insert_data, string filepath, int tuples_limit) {
+    istringstream del_iss(insert_data);
+    string data = "";
+    del_iss >> data;
+    del_iss >> data;
+    del_iss >> data;
+    SelectedTables del_tabl;
+    FROM(nullptr, filepath, data, del_tabl);
+    del_iss >> data;
+    string copy_data = del_tabl.head->table_data;
+    if (data == "WHERE") {
+        WHERE(copy_data, insert_data);
+    }
+    CutOff(del_tabl.head->table_data, copy_data);
+    replaceTable(del_tabl.head->table_data, '\"' + filepath + "\"/" + del_tabl.head->tabl_name, tuples_limit);
+}
+
 void EnteringCommand(string input_data, Schema sch) {
     istringstream read_command(input_data);
     string command_word = "";
@@ -507,10 +660,12 @@ void EnteringCommand(string input_data, Schema sch) {
     try {
         if (command_word == "INSERT") INSERT(input_data, sch.tuples_limit, sch.name);
         if (command_word == "SELECT") SELECT(input_data, sch.name);
+        if (command_word == "DELETE") DELETE(input_data, sch.name, sch.tuples_limit);
     } catch (string& error) {
         throw string(error);
     }
 }
+
 
 int main() {
     string result = ReadingJson();
@@ -518,11 +673,13 @@ int main() {
     int index = 0;
     string words = "";
     Schema sch = FillSchema(result);
-    cout << result << "\Enter your command: " << endl;
-    //CreateFiles(sch);
-    //INSERT("INSERT INTO таблица1 VALUES('1', '2', '3', '4')", sch.tuples_limit, sch.name);
+    string choix = "";
+    cout << result  <<"\nIf you want to update your schema enter 1" << endl;
+    cin >> choix;
+    if (choix == "1") CreateFiles(sch);
+    cout << "Enter your command: " << endl;
     string command = "";
-    while (command != "end") {
+    while (command != "quit") {
         try {
             getline(cin, command);
             EnteringCommand(command, sch);
